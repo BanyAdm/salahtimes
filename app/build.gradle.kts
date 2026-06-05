ea3635d06bf7ef5aa79e97f2e58d5478096ee52e
BanyAdm <sohaibextra69@gmail.com>
2026-06-05 14:35:33 +0300

    Add coroutines-play-services dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0")
     implementation("androidx.core:core-ktx:1.13.0")
 }
